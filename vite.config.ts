6659c82cf47310946494aa6c7fa0a2e146cd0b9f
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-09-12 21:44:02 -0700

    build: add Cloudflare build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
+
 import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
   plugins: [
@@ -13,6 +14,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart({
       customViteReactPlugin: true,
+      target: 'cloudflare-module',
     }),
     viteReact(),
   ],
